6801ad549ab3b4bdf2a0e08f53699d6de2860ec7
Xinox3213 <mavrinonlywork@gmail.com>
2025-08-19 21:43:41 +0700

    написал базу данных

---
+++ db/src/main/java/ru/mavrinvladislav/db/AppDataBase.kt
@@ -0,0 +1,34 @@
+package ru.mavrinvladislav.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+
+        private const val DB_NAME = "main.db"
+        private var instance: AppDatabase? = null
+        private val MONITOR = Any()
+
+        fun getInstance(context: Context): AppDatabase {
+            instance?.let {
+                return it
+            }
+            synchronized(MONITOR) {
+                val db = Room.databaseBuilder(
+                    context = context,
+                    klass = AppDatabase::class.java,
+                    name = DB_NAME
+                ).build()
+                instance = db
+                return db
+            }
+        }
+    }
+}
