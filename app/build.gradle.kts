4013c25ef60fe542532b895e0b95a083fc26bf00
Xinox3213 <mavrinonlywork@gmail.com>
2025-08-24 14:55:04 +0700

    очистил app от лишнего

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,20 +44,12 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(project(":core:network"))
     implementation(project(":core:system-design"))
@@ -65,14 +57,6 @@ dependencies {
     implementation(project(":core:di"))
     implementation(project(":db"))
     implementation(project(":feature:user"))
-
-
-//Mvi kotlin
-    implementation(libs.mvikotlin.main)
-    implementation(libs.mvikotlin.core)
-    implementation(libs.mvikotlin.coroutines)
-
-
 
     ksp(libs.dagger.compiler)
 }
